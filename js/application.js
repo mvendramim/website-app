9e2848a8e48d52ac24ef99910cd961f30cc1c31d
Leonardo <leonardowistuba@gmail.com>
2014-04-26 23:39:54 -0300

    Faz mudar a cor do menu quando ele ultrapassa o topo da segunda página.

--- js/application.js
+++ js/application.js
@@ -1,6 +1,32 @@
 $(document).ready(function() {
 	$('#nav').onePageNav();
+
+    listenScrollToChangeMenuColor();
 });
+
+function listenScrollToChangeMenuColor()
+{
+  var $homePage = $('#home');
+
+  var $lastMenuItem = $('#menu-last-item');
+
+  $(window).on('scroll',function() {
+
+    var lastMenuTop = $lastMenuItem.offset().top;
+    var secondPageTop = $homePage.height();
+
+    if (lastMenuTop > secondPageTop) {
+        $('.nav-text').addClass('nav-text-selected');
+        $('.item').addClass('item-red');
+        $('.current').addClass('selected-item-red');
+
+    } else {
+        $('.nav-text').removeClass('nav-text-selected');
+        $('.item').removeClass('item-red');
+        $('.current').removeClass('selected-item-red');
+    }
+  });
+};
 
 $('#nav').onePageNav({
     currentClass: 'current',
@@ -53,3 +79,7 @@ $(window).on('scroll',function(){
 
 
 });
+
+function colorMenu() {
+
+};
