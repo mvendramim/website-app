14184a036272cfafd9449855f26be2d86fae3289
Mayra Vendramim <may.vendramim@gmail.com>
2014-04-26 22:35:16 -0400

    Lightbox

--- js/application.js
+++ js/application.js
@@ -1,25 +1,14 @@
 $(document).ready(function() {
-	$('#nav').onePageNav();
-});
+    $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
 
-$('#nav').onePageNav({
-    currentClass: 'current',
-    changeHash: false,
-    scrollSpeed: 750,
-    scrollThreshold: 0.5,
-    filter: '',
-    easing: 'swing',
-    begin: function() {
-        //I get fired when the animation is starting
-    },
-    end: function() {
-        //I get fired when the animation is ending
-    },
-    scrollChange: function($currentListItem) {
-        //I get fired when you enter a section and I pass the list item of the section
-    }
+        fixedContentPos: false
+    });
 });
-
 
 var mainbottom = 550;
 var logo = 700;
