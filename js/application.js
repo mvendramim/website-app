0a7b6f781cb9f51ec4f2ad8c218eb59078e2a894
Leonardo <leonardowistuba@gmail.com>
2014-04-27 23:42:49 -0300

    Arruma menu com one-page-nav.

--- js/application.js
+++ js/application.js
@@ -15,7 +15,13 @@ $(document).ready(function() {
             //I get fired when the animation is ending
         },
         scrollChange: function($currentListItem) {
-            //I get fired when you enter a section and I pass the list item of the section
+            $liSecondScreen = $('#li-second-screen');
+            if ($currentListItem.attr('id') === $liSecondScreen.attr('id')) {
+                console.log('banana');
+                $('.item').addClass('red');
+            } else {
+                $('.item').removeClass('red');
+            }
         }
     });
 });
@@ -45,14 +51,14 @@ function listenScrollToChangeMenuColor()
     var secondPageTop = $homePage.height();
 
     if (lastMenuTop > secondPageTop) {
-        $('.nav-text').addClass('nav-text-selected');
-        $('.item').addClass('item-red');
-        $('.current').addClass('selected-item-red');
+        // $('.nav-text').addClass('nav-text-selected');
+        // $('.item').addClass('item-red');
+        // $('.current').addClass('selected-item-red');
 
     } else {
-        $('.nav-text').removeClass('nav-text-selected');
-        $('.item').removeClass('item-red');
-        $('.current').removeClass('selected-item-red');
+        // $('.nav-text').removeClass('nav-text-selected');
+        // $('.item').removeClass('item-red');
+        // $('.current').removeClass('selected-item-red');
     }
   });
 };
@@ -63,24 +69,24 @@ var mainbottom = 550;
  
 $(window).on('scroll',function(){
     
-    stop = Math.round($(window).scrollTop());
-    if (stop > mainbottom) {
-        $('.nav-text').addClass('nav-text-selected');
-    } else {
-        $('.nav-text').removeClass('nav-text-selected');
-   }
+   //  stop = Math.round($(window).scrollTop());
+   //  if (stop > mainbottom) {
+   //      $('.nav-text').addClass('nav-text-selected');
+   //  } else {
+   //      $('.nav-text').removeClass('nav-text-selected');
+   // }
     
-    if (stop > mainbottom) {
-        $('.item').addClass('item-red');
-    } else {
-        $('.item').removeClass('item-red');
-   }
+   //  if (stop > mainbottom) {
+   //      $('.item').addClass('item-red');
+   //  } else {
+   //      $('.item').removeClass('item-red');
+   // }
     
-    if (stop > mainbottom) {
-        $('.current').addClass('selected-item-red');
-    } else {
-        $('.current').removeClass('selected-item-red');
-   }
+   //  if (stop > mainbottom) {
+   //      $('.current').addClass('selected-item-red');
+   //  } else {
+   //      $('.current').removeClass('selected-item-red');
+   // }
     
 
 
